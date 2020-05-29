c3ba944fd528d42644ee9a15ef644094d53cb67f
Daniel Kwon <danielkwon89@gmail.com>
2020-05-29 22:29:27 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -21,19 +21,37 @@ class Transfer
     elsif !self.valid?
       self.status = "rejected"
       "Transaction rejected. Please check your account balance."
+<<<<<<< HEAD
     elsif self.status != "complete"
       sender.balance -= amount
       receiver.balance += amount
+=======
+    else
+      sender.balance -= amount
+      receiver.balance += amount
+      self.last_amount = self.amount
+      self.amount = 0
+>>>>>>> f9cd01a15330b409a7f5e3c2cbd263add21c97bd
       self.status = "complete"
     end
   end
   
   def reverse_transfer
+<<<<<<< HEAD
     if self.status == "complete"
       binding.pry
       self.receiver.balance -= amount
       self.sender.balance += amount
       self.status = "reversed"
     end
+=======
+    unless self.status == "reversed"
+    binding.pry
+      self.receiver.balance -= self.last_amount
+      self.sender.balance += self.last_amount
+      self.status = "reversed"
+    end
+    
+>>>>>>> f9cd01a15330b409a7f5e3c2cbd263add21c97bd
   end
 end
